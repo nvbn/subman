5cc116c8a0e7d4aa27b1da0381de82b990592036
nvbn <nvbn.rm@gmail.com>
2014-01-24 05:16:19 +0400

    Improve search query

--- src/subman/helpers.clj
+++ src/subman/helpers.clj
@@ -3,3 +3,7 @@
 (defn print-first [arg fnc & args]
   (println arg)
   (apply fnc arg args))
+
+(defn remove-first-0
+  "Remove first 0 from string"
+  [query] (clojure.string/replace query #"^(0+)" ""))
--- src/subman/models.clj
+++ src/subman/models.clj
@@ -2,7 +2,8 @@
   (:require [clojurewerkz.elastisch.rest :as esr]
             [clojurewerkz.elastisch.rest.index :as esi]
             [clojurewerkz.elastisch.rest.document :as esd]
-            [clojurewerkz.elastisch.query :as q]))
+            [clojurewerkz.elastisch.query :as q]
+            [subman.helpers :as helpers]))
 
 (esr/connect! "http://127.0.0.1:9200")
 
@@ -27,15 +28,34 @@
   "Delete all documents"
   [] (esd/delete-by-query-across-all-types index (q/match-all)))
 
-(defn- prepare-query
-  "Prepare query for search"
+(defn- remove-dots
+  "Remove dots from query"
   [query] (clojure.string/replace query #"\." " "))
+
+(defn- build-fuzzy
+  "Build default fuzzy query"
+  [query] {:query (q/fuzzy-like-this :like_text query)})
+
+
+(defn- add-season-episode
+  "Add season and episode filters"
+  [query text] (if-let [nums (re-find #"[sS](\d+)[eE](\d+)" text)]
+                 (assoc query :filter {:term {:season (helpers/remove-first-0 (get nums 1))
+                                              :episode (helpers/remove-first-0 (get nums 2))}})
+                 query))
+
+(defn- build-query
+  "Build search query"
+  [query] (-> (remove-dots query)
+              build-fuzzy
+              (add-season-episode query)
+              (assoc :size 100)
+              vec
+              flatten))
 
 (defn search
   "Search for documents"
-  [query] (->> (esd/search index "subtitle"
-                           :query (q/fuzzy-like-this :like_text (prepare-query query))
-                           :size 100)
+  [query] (->> (apply esd/search index "subtitle" (build-query query))
                :hits
                :hits
                (map :_source)))
