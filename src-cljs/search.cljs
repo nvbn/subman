07daec4e80025c85e20e1b10ddd8ccf63afd185a
nvbn <nvbn.rm@gmail.com>
2014-02-11 23:24:03 +0400

    Remove old views

--- src-cljs/search.cljs
+++ src-cljs/search.cljs
@@ -1,9 +1,9 @@
 (ns subman.search
   (:use [cljs.reader :only [read-string]]
+        [reagent.core :only [atom]]
         [subman.helpers :only [is-filled?]])
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [reagent.core :as reagent :refer [atom]]
-            [cljs-http.client :as http]
+  (:require [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
             [subman.const :as const]))
 
--- src/subman/views.clj
+++ src/subman/views.clj
@@ -11,28 +11,4 @@
           (include-js "/components/jquery/jquery.js"
                       "/components/bootstrap/dist/js/bootstrap.js"
                       "/main.js")]
-         [:body
-          [:div.input-group.input-group-lg.col-xs-12 {:data-spy "affix"
-                                                      :data-offset-top "40"
-                                                      :style "z-index: 100"}
-           [:span.input-group-addon [:i.fa.fa-search]]
-           [:input.form-control {:type "text"
-                                 :placeholder "Type search query"
-                                 :ng-model "query"
-                                 :ng-change "updateFilter()"}]]
-          [:div.container.col-xs-12
-           [:div.search-result-list.list-group
-            [:a.list-group-item.search-result {:href "{{result.url}}"
-                                               :target "_blank"
-                                               :ng-repeat "result in results"}
-             [:h3
-              "{{result.show}}"
-              [:span {:ng-show "result.name"} " - {{result.name}}"]
-              [:span {:ng-show "result.season || result.episode"}
-               " (" [:span {:ng-show "result.season"} "S{{result.season}}"]
-               [:span {:ng-show "result.episode"} "E{{result.episode}}"]
-               ")"]]
-             [:p.pull-right "Source: {{{0: 'Addicted', 1: 'Podnapisi'}[result.source]}}"]
-             [:p "Language: {{result.lang}}"]
-             [:p {:ng-show "result.version"} "Version: {{result.version}}"]]]]
-          [:script "subman.core.run();"]]))
+         [:body [:script "subman.core.run();"]]))
