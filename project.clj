b2837859510bfa673cdaeace8bd1b0cb461c11a4
nvbn <nvbn.rm@gmail.com>
2014-02-11 23:56:19 +0400

    Change project structure

--- project.clj
+++ project.clj
@@ -21,12 +21,13 @@
             [lein-cljsbuild "1.0.1"]
             [com.keminglabs/cljx "0.3.2"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]]}}
-  :cljsbuild {:builds [{:source-paths ["src-cljs" "target/generated-cljs"]
+  :source-paths ["src/clj"]
+  :cljsbuild {:builds [{:source-paths ["src/cljs" "target/generated-cljs"]
                         :compiler {:preamble ["reagent/react.js"]
                                    :output-to "resources/public/main.js"}}]}
-  :cljx {:builds [{:source-paths ["src-cljx"]
+  :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/classes"
                    :rules :clj}
-                  {:source-paths ["src-cljx"]
+                  {:source-paths ["src/cljx"]
                    :output-path "target/generated-cljs"
                    :rules :cljs}]})
