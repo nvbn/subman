adaa3c042b25bc42a1f79846664904cf46c1b5f3
nvbn <nvbn.rm@gmail.com>
2014-02-19 00:58:55 +0400

    Add tests for filler

---
+++ test/subman/t_filler.clj
@@ -0,0 +1,32 @@
+(ns subman.t-filler
+  (:require [midje.sweet :refer [fact facts => provided anything truthy]]
+            [clojure.data.json :as json]
+            [subman.models :as models]
+            [subman.sources.addicted :as addicted]
+            [subman.sources.podnapisi :as podnapisi]
+            [subman.sources.opensubtitles :as opensubtitles]
+            [subman.sources.subscene :as subscene]
+            [subman.filler :as filler]))
+
+(defn new-getter
+  "Fake getter for tests"
+  [page] (case page
+           3 [:exists]
+           2 [:fresh :exists]
+           1 [:fresh :fresh]))
+
+(fact "should get new before"
+      (-> (filler/get-new-before new-getter #(= % :exists))
+          count) => 3)
+
+(fact "should get all new from nth getters"
+      (-> (filler/get-all-new #(= % :exists) new-getter new-getter new-getter)
+          count) => 9)
+
+(fact "update from all sources"
+      (filler/update-all) => truthy
+      (provided
+       (subscene/get-release-page-result 1) => []
+       (opensubtitles/get-release-page-result 1) => []
+       (addicted/get-release-page-result 1) => []
+       (podnapisi/get-release-page-result 1) => []))
