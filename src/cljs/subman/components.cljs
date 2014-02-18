386e53ffd83963e204df72ec20d26960578fc422
nvbn <nvbn.rm@gmail.com>
2014-02-18 19:38:46 +0400

    Split components from logic on client side

---
+++ src/cljs/subman/components.cljs
@@ -0,0 +1,66 @@
+(ns subman.components
+  (:require [subman.const :as const]
+            [subman.helpers :refer [is-filled?]]))
+
+(defn search-box
+  "Search box"
+  [{:keys [value]}] [:div.input-group.input-group-lg.col-xs-12.search-input
+                     {:data-spy "affix"
+                      :data-offset-top "40"}
+                     [:span.input-group-addon (if (= "" @value)
+                                                [:i.fa.fa-search]
+                                                [:a {:on-click (fn [e]
+                                                                 (.preventDefault e)
+                                                                 (reset! value ""))
+                                                     :href "#"}
+                                                 [:i.fa.fa-chevron-left]])]
+                     [:input.form-control {:type "text"
+                                           :placeholder "Type search query"
+                                           :on-change #(reset! value (-> % .-target .-value))
+                                           :value @value}]])
+
+(defn result-line
+  "Search result line"
+  [{:keys [url name show season episode source lang version]}]
+  [:a.list-group-item.search-result {:href url
+                                     :target "_blank"}
+   [:h3
+    show (when (is-filled? name) (str " - " name))
+    (when (some is-filled? [season episode])
+      [:span " " (when (is-filled? season) (str "S" season))
+       (when (is-filled? episode) (str "E" episode))])]
+   [:p.pull-right "Source: " (const/type-names source)]
+   [:p "Language: " lang]
+   (when (is-filled? version)
+     [:p "Version: " version])])
+
+(defn result-list
+  "Search result list"
+  [{:keys [query items counter total-count]}]
+  (cond
+   (> (count @items) 0) [:div.container.col-xs-12.search-result-holder
+                         [:div.search-result-list.list-group (map result-line @items)]]
+   (or (= @counter 0)
+       (= (count @query) 0)) [:div.container.col-xs-12.info-box
+                              [:h2 "Welcome to subman!"]
+                              [:p "We indexing "
+                               [:a {:href "http://www.addic7ed.com/"
+                                    :target "_blank"} "addic7ed.com"]
+                               ", "
+                               [:a {:href "http://www.opensubtitles.org/"
+                                    :target "_blank"} "www.opensubtitles.org"]
+                               ", "
+                               [:a {:href "http://www.podnapisi.net/"
+                                    :target "_blank"} "podnapisi.net"]
+                               " and "
+                               [:a {:href "http://subscene.com/"
+                                    :target "_blank"} "subscene.com"]
+                               "."]
+                              [:p "Total indexed subtitles count: "
+                               @total-count
+                               "."]
+                              [:a {:href "https://github.com/nvbn/subman"
+                                   :target "_blank"}
+                               [:i.fa.fa-github] " github"]]
+   :else [:div.container.col-xs-12.info-box
+          [:h2 "Nothing found for \"" @query "\""]]))
--- src/cljs/subman/search.cljs
+++ src/cljs/subman/search.cljs
@@ -5,73 +5,9 @@
             [reagent.core :refer [atom]]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
-            [subman.const :as const]
-            [subman.helpers :refer [is-filled?]]
             [subman.history :refer [init-history]]
-            [subman.push :refer [init-push]]))
-
-(defn search-box
-  "Search box"
-  [{:keys [value]}] [:div.input-group.input-group-lg.col-xs-12.search-input
-                     {:data-spy "affix"
-                      :data-offset-top "40"}
-                     [:span.input-group-addon (if (= "" @value)
-                                                [:i.fa.fa-search]
-                                                [:a {:on-click (fn [e]
-                                                                 (.preventDefault e)
-                                                                 (reset! value ""))
-                                                     :href "#"}
-                                                 [:i.fa.fa-chevron-left]])]
-                     [:input.form-control {:type "text"
-                                           :placeholder "Type search query"
-                                           :on-change #(reset! value (-> % .-target .-value))
-                                           :value @value}]])
-
-(defn result-line
-  "Search result line"
-  [{:keys [url name show season episode source lang version]}]
-  [:a.list-group-item.search-result {:href url
-                                     :target "_blank"}
-   [:h3
-    show (when (is-filled? name) (str " - " name))
-    (when (some is-filled? [season episode])
-      [:span " " (when (is-filled? season) (str "S" season))
-       (when (is-filled? episode) (str "E" episode))])]
-   [:p.pull-right "Source: " (const/type-names source)]
-   [:p "Language: " lang]
-   (when (is-filled? version)
-     [:p "Version: " version])])
-
-(defn result-list
-  "Search result list"
-  [{:keys [query items counter total-count]}]
-  (cond
-   (> (count @items) 0) [:div.container.col-xs-12.search-result-holder
-                         [:div.search-result-list.list-group (map result-line @items)]]
-   (or (= @counter 0)
-       (= (count @query) 0)) [:div.container.col-xs-12.info-box
-                              [:h2 "Welcome to subman!"]
-                              [:p "We indexing "
-                               [:a {:href "http://www.addic7ed.com/"
-                                    :target "_blank"} "addic7ed.com"]
-                               ", "
-                               [:a {:href "http://www.opensubtitles.org/"
-                                    :target "_blank"} "www.opensubtitles.org"]
-                               ", "
-                               [:a {:href "http://www.podnapisi.net/"
-                                    :target "_blank"} "podnapisi.net"]
-                               " and "
-                               [:a {:href "http://subscene.com/"
-                                    :target "_blank"} "subscene.com"]
-                               "."]
-                              [:p "Total indexed subtitles count: "
-                               @total-count
-                               "."]
-                              [:a {:href "https://github.com/nvbn/subman"
-                                   :target "_blank"}
-                               [:i.fa.fa-github] " github"]]
-   :else [:div.container.col-xs-12.info-box
-          [:h2 "Nothing found for \"" @query "\""]]))
+            [subman.push :refer [init-push]]
+            [subman.components :as components]))
 
 (defn create-search-request
   "Create search request from query"
@@ -109,8 +45,8 @@
        (update-total-count total-count)
        (init-history query)
        (init-push total-count)
-       [:div [search-box {:value query}]
-        [result-list {:items results
+       [:div [components/search-box {:value query}]
+        [components/result-list {:items results
                       :query query
                       :counter counter
                       :total-count total-count}]]))
