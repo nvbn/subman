55c497186717914901e5d589dc1269fa74c5ab44
nvbn <nvbn.rm@gmail.com>
2014-02-19 05:49:43 +0400

    Do kibit recomendations

--- test/clj/subman/t_filler.clj
+++ test/clj/subman/t_filler.clj
@@ -16,12 +16,14 @@
     1 [:fresh :fresh]))
 
 (fact "should get new before"
-      (-> (filler/get-new-before new-getter #(= % :exists))
-          count) => 3)
+      (count (filler/get-new-before new-getter
+                                    #(= % :exists))) => 3)
 
 (fact "should get all new from nth getters"
-      (-> (filler/get-all-new #(= % :exists) new-getter new-getter new-getter)
-          count) => 9)
+      (count (filler/get-all-new #(= % :exists)
+                                 new-getter
+                                 new-getter
+                                 new-getter)) => 9)
 
 (fact "update from all sources"
       (filler/update-all) => truthy
