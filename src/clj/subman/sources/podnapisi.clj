1ff692e8056cee2fb7abe21ce9fdb10f97e1753d
nvbn <nvbn.rm@gmail.com>
2014-02-19 05:37:35 +0400

    Make little refactoring on podnapisi source

--- src/clj/subman/sources/podnapisi.clj
+++ src/clj/subman/sources/podnapisi.clj
@@ -6,13 +6,6 @@
 
 (defn- make-url [url] (str "http://www.podnapisi.net" url))
 
-(defn- download-element
-  "Get download element"
-  [item]
-  (-> item
-      (html/select [[:td html/first-child] :a.subtitle_page_link])
-      first))
-
 (defn- season-episode-part
   "Get item from season episode part"
   [item pos]
@@ -26,33 +19,53 @@
                                 clojure.string/trim
                                 helpers/remove-first-0)))
 
+(defn- get-show
+  "Get show from download item"
+  [download-element]
+  (-> (:content download-element)
+      first
+      clojure.string/trim))
+
+(defn- get-url
+  "Get url from download item"
+  [download-element]
+  (-> download-element
+      :attrs
+      :href
+      make-url))
+
+(defn- get-version
+  "Get version from line"
+  [item]
+  (-> (html/select item [[:td html/first-child] :span.release])
+      first
+      :content
+      last))
+
+(defn- get-lang
+  "Get lang from line"
+  [item]
+  (-> (html/select item [[:td (html/nth-child 3)]
+                         :div.flag])
+      first
+      :attrs
+      :alt
+      (clojure.string/split #" ")
+      first))
+
 (defn- create-subtitle-map
   "Create subtitle map from list page item"
   [item]
-  {:show (-> (download-element item)
-             :content
-             first
-             clojure.string/trim)
-   :url (-> (download-element item)
-            :attrs
-            :href
-            make-url)
-   :season (season-episode-part item 1)
-   :episode (season-episode-part item 2)
-   :version (-> item
-                (html/select [[:td html/first-child] :span.release])
-                first
-                :content
-                last)
-   :name ""
-   :lang (-> item
-             (html/select [[:td (html/nth-child 3)]
-                           :div.flag])
-             first
-             :attrs
-             :alt
-             (clojure.string/split #" ")
-             first)})
+  (let [download-element (first
+                          (html/select item [[:td html/first-child]
+                                             :a.subtitle_page_link]))]
+    {:show (get-show download-element)
+     :url (get-url download-element)
+     :season (season-episode-part item 1)
+     :episode (season-episode-part item 2)
+     :version (get-version item)
+     :name ""
+     :lang (get-lang item)}))
 
 (defn- parse-list-page
   "Parse page with subtitles list"
--- test/clj/subman/sources/t_podnapisi.clj
+++ test/clj/subman/sources/t_podnapisi.clj
@@ -5,35 +5,54 @@
             [subman.helpers :as helpers :refer [get-from-file get-from-line]]
             [subman.const :as const]))
 
+(def line-content
+  (get-from-file "test/fixtures/subman/sources/podnapisi_line.html"))
+
+(def release-content
+  (get-from-file
+   "test/fixtures/subman/sources/podnapisi_release.html"))
+
 (fact "should make absolute url"
       (#'podnapisi/make-url "/test") => "http://www.podnapisi.net/test")
 
 (facts "get item from season-episode part"
        (fact "when can"
-             (#'podnapisi/season-episode-part
-              (get-from-file "test/fixtures/subman/sources/podnapisi_line.html")
-              2) => "6")
+             (#'podnapisi/season-episode-part line-content 2) => "6")
        (fact "when can't"
              (#'podnapisi/season-episode-part
               (get-from-line "<td></td>") 2) => ""))
 
+(fact "should get show"
+      (#'podnapisi/get-show (-> (get-from-line "<b> test </b>")
+                                (html/select [:b])
+                                first)) => "test")
+
+(fact "should get url"
+      (#'podnapisi/get-url (-> (get-from-line "<a href='/test'></a>")
+                               (html/select [:a])
+                               first)) => "http://www.podnapisi.net/test")
+
+(fact "should get version"
+      (#'podnapisi/get-version line-content) => "Bitten.S01E06.HDTV.x264-2HD\nBitten.S01E06.HDTV.Xv...")
+
+(fact "should get language"
+      (#'podnapisi/get-lang line-content) => "English")
+
 (fact "should create subtitle map from line"
       (#'podnapisi/create-subtitle-map
-       (get-from-file
-        "test/fixtures/subman/sources/podnapisi_line.html")) => {:episode "6"
-                                                                 :lang "English"
-                                                                 :name ""
-                                                                 :season "1"
-                                                                 :show "Bitten"
-                                                                 :url "http://www.podnapisi.net/ru/bitten-2014-subtitles-p2920797"
-                                                                 :version "Bitten.S01E06.HDTV.x264-2HD\nBitten.S01E06.HDTV.Xv..."})
+       line-content) => {:episode "6"
+                         :lang "English"
+                         :name ""
+                         :season "1"
+                         :show "Bitten"
+                         :url "http://www.podnapisi.net/ru/bitten-2014-subtitles-p2920797"
+                         :version "Bitten.S01E06.HDTV.x264-2HD\nBitten.S01E06.HDTV.Xv..."})
 
 (fact "should get subtitle maps from release page"
       (-> (#'podnapisi/parse-list-page "")
           first
           :show) => "Lab Rats"
-      (provided (helpers/fetch anything) => (get-from-file
-                                             "test/fixtures/subman/sources/podnapisi_release.html")))
+      (provided (helpers/fetch anything) => release-content))
 
 (fact "should get release page url"
       (#'podnapisi/get-release-page-url 1) => "http://www.podnapisi.net/en/ppodnapisi/search/sJ/-1/sS/time/sO/desc/sT/-1/sM/0/sA/0/sK//sOA/0/sOT/0/sOL/0/sOI/0/sOE/0/sOD/0/sOH/0/sY//sOCS/0/sFT/0/sR//sTS//sTE//sAKA/1/sH//sI//tbsl/1/asdp/0/page//page/1")
@@ -42,5 +61,4 @@
       (-> (#'podnapisi/get-release-page-result 1)
           first
           :source) => const/type-podnapisi
-      (provided (helpers/fetch anything) => (get-from-file
-                                             "test/fixtures/subman/sources/podnapisi_release.html")))
+      (provided (helpers/fetch anything) => release-content))
