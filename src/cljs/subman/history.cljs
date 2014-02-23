d8770f4407eda4c08c8d0c953f2f67c80980930d
nvbn <nvbn.rm@gmail.com>
2014-02-24 03:37:11 +0400

    Make little refactoring on history

--- src/cljs/subman/history.cljs
+++ src/cljs/subman/history.cljs
@@ -13,20 +13,30 @@
     (.setPathPrefix "/search/")
     (.setEnabled true)))
 
+(defn watch-to-value
+  "Watch to search result value"
+  [history value]
+  (add-watch value :history
+             (fn [_ _ _ new-value]
+               (wait 500 #(when (= new-value @value)
+                            (.setToken history new-value))))))
+
+(defn watch-to-navigation
+  "Watch to navigation events"
+  [history value]
+  (gevents/listen history history-event/NAVIGATE
+                  #(let [token (-> % .-token js/decodeURIComponent)]
+                     (when (and (= (.-isNavigatopn %))
+                                (not= token @value))
+                       (reset! value token)))))
+
 (defn init-history
   "Init history and spy to atom"
   [value]
   (when (history5/isSupported)
     (let [history (get-history)]
-      (add-watch value :history
-                 (fn [_ _ _ new-value]
-                   (wait 500 #(when (= new-value @value)
-                                (.setToken history new-value)))))
-      (gevents/listen history history-event/NAVIGATE
-                      #(let [token (-> % .-token js/decodeURIComponent)]
-                         (when (and (= (.-isNavigatopn %))
-                                    (not= token @value))
-                           (reset! value token))))
+      (watch-to-value history value)
+      (watch-to-navigation history value)
       (->> (.getToken history)
            js/decodeURIComponent
            (reset! value)))))
