fa20ceb04471815dd5978183691710fc9ee4e7c3
nvbn <nvbn.rm@gmail.com>
2014-02-24 03:52:48 +0400

    Upgrade reagent and clojurescript

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
                  [enlive "1.1.5"]
                  [clojurewerkz/elastisch "1.5.0-beta1"]
-                 [reagent "0.3.0"]
+                 [reagent "0.4.1"]
                  [cljs-http "0.1.7"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [overtone/at-at "1.2.0"]
