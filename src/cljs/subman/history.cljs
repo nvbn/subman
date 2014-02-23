1ee13449a97d3fc24e956e73df0094f01f781f7e
nvbn <nvbn.rm@gmail.com>
2014-02-24 03:19:51 +0400

    Not change url when typing

--- src/cljs/subman/history.cljs
+++ src/cljs/subman/history.cljs
@@ -3,7 +3,8 @@
             [goog.events :as gevents]
             [goog.history.EventType :as history-event]
             [goog.string :as string]
-            [goog.history.Html5History :as history5]))
+            [goog.history.Html5History :as history5]
+            [jayq.util :refer [wait]]))
 
 (defn get-history
   "Get html5 history obj or fallback"
@@ -23,11 +24,12 @@
     (let [history (get-history)]
       (add-watch value :history
                  (fn [_ _ _ new-value]
-                   (.setToken history new-value)))
+                   (wait 500 #(when (= new-value @value)
+                                (.setToken history new-value)))))
       (gevents/listen history history-event/NAVIGATE
                       #(let [token (.-token %)]
                          (when-not (= token @value)
-                           (reset! value (.-token %)))))
+                           (reset! value token))))
       (->> (.getToken history)
            string/urlDecode
            (reset! value)))))
