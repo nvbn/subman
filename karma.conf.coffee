54b290d70265e5dfa03e7afcaf113bb297f3818d
nvbn <nvbn.rm@gmail.com>
2014-02-24 03:43:24 +0400

    Fix client side tests

--- karma.conf.coffee
+++ karma.conf.coffee
@@ -4,4 +4,6 @@ module.exports = (config) ->
     autoWatch: true
     reporters: ['progress']
     frameworks: ['jasmine']
-    files: ['target/karma-test.js']
+    files: ["resources/public/components/jquery/dist/jquery.js"
+            "resources/public/components/bootstrap/dist/js/bootstrap.js"
+            "target/karma-test.js"]
--- test/cljs/t_search.cljs
+++ test/cljs/t_search.cljs
@@ -6,12 +6,16 @@
 
 (facts "should create search request"
        (fact "with query"
-             (search/create-search-request "test") => "/api/search/?query=test")
+             (search/create-search-request "test" 0) => "/api/search/?query=test&offset=0")
        (fact "with query and lang"
              (search/create-search-request
-              "test :lang ru") => "/api/search/?query=test&lang=ru"))
+              "test :lang ru" 0) => "/api/search/?query=test&lang=ru&offset=0")
+       (fact "with offset"
+             (search/create-search-request
+              "test" 100) => "/api/search/?query=test&offset=100"))
 
 (fact "watch to query should be ok"
       (search/watch-to-query (atom "")
                              (atom [])
+                             (atom 0)
                              (atom 0)) => truthy)
