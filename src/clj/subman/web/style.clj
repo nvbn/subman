e10346d170db4b9c063e455d558729178b95c7a6
nvbn <nvbn.rm@gmail.com>
2014-04-09 00:34:57 +0400

    Impove search result view

--- src/clj/subman/web/style.clj
+++ src/clj/subman/web/style.clj
@@ -1,8 +1,10 @@
 (ns subman.web.style
-  (:require [garden.def :refer [defstyles]]))
+  (:require [garden.def :refer [defstyles]]
+            [garden.units :refer [px]]))
 
 (defstyles main
   [:.search-input {:z-index 100}]
   [:.info-box {:text-align "center"}]
   [:.search-result-holder {:padding-left 0
-                           :padding-right 0}])
+                           :padding-right 0}]
+  [:.search-result-list {:margin-top (px -5)}])
