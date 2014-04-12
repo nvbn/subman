8a15685a0110e96136f65d09eb714f58bba579dc
nvbn <nvbn.rm@gmail.com>
2014-04-12 23:51:49 +0400

    Improve local cljs compilation speed

--- project.clj
+++ project.clj
@@ -30,8 +30,6 @@
                                {:main {:source-paths ["src/cljs" "target/generated-cljs"]
                                        :compiler {:preamble ["reagent/react.js"]
                                                   :output-to "resources/public/main.js"
-                                                  :pretty-print true
-                                                  :optimizations :simple
                                                   :output-dir "resources/public/cljs-target"}}
                                 :test {:source-paths ["src/cljs" "test/cljs"
                                                       "target/generated-cljs"]
