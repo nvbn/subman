5d947b438886fd26e86a74a9275584b982c20cc5
nvbn <nvbn.rm@gmail.com>
2014-08-23 21:30:57 +0900

    Move css and js vectors to separate vars

--- src/clj/subman/web/views.clj
+++ src/clj/subman/web/views.clj
@@ -3,6 +3,35 @@
             [environ.core :refer [env]]
             [subman.helpers :refer [as-static make-static]]))
 
+(def debug-js
+  ["components/jquery/dist/jquery.js"
+   "components/bootstrap/dist/js/bootstrap.js"
+   "components/typeahead.js/dist/typeahead.jquery.js"
+   "components/react/react.js"
+   "cljs-target/goog/base.js"
+   "main.js"])
+
+(def production-js
+  ["components/jquery/dist/jquery.min.js"
+   "components/bootstrap/dist/js/bootstrap.min.js"
+   "components/typeahead.js/dist/typeahead.jquery.min.js"
+   "components/react/react.min.js"
+   "main.js"])
+
+(def debug-css
+  ["components/bootstrap/dist/css/bootstrap.css"
+   "components/bootstrap/dist/css/bootstrap-theme.css"
+   "components/font-awesome/css/font-awesome.css"
+   "components/typeahead.js-bootstrap3.less/typeahead.css"
+   "main.css"])
+
+(def production-css
+  ["components/bootstrap/dist/css/bootstrap.min.css"
+   "components/bootstrap/dist/css/bootstrap-theme.min.css"
+   "components/font-awesome/css/font-awesome.min.css"
+   "components/typeahead.js-bootstrap3.less/typeahead.css"
+   "main.css"])
+
 (defn index-page []
   (let [is-debug (env :is-debug)]
     (html5 [:head
@@ -13,34 +42,11 @@
                     :rel  "search"
                     :href (first (make-static "opensearch.xml"))}]
             [:title "Subman - subtitle search service"]
-            (when-not is-debug
-              (as-static include-css
-                         "components/bootstrap/dist/css/bootstrap.min.css"
-                         "components/bootstrap/dist/css/bootstrap-theme.min.css"
-                         "components/font-awesome/css/font-awesome.min.css"
-                         "components/typeahead.js-bootstrap3.less/typeahead.css"
-                         "main.css"))
-            (when-not is-debug
-              (as-static include-js
-                         "components/jquery/dist/jquery.min.js"
-                         "components/bootstrap/dist/js/bootstrap.min.js"
-                         "components/typeahead.js/dist/typeahead.jquery.min.js"
-                         "components/react/react.min.js"
-                         "main.js"))
-            (when is-debug
-              (as-static include-css
-                         "components/bootstrap/dist/css/bootstrap.css"
-                         "components/bootstrap/dist/css/bootstrap-theme.css"
-                         "components/font-awesome/css/font-awesome.css"
-                         "components/typeahead.js-bootstrap3.less/typeahead.css"
-                         "main.css"))
-            (when is-debug
-              (as-static include-js
-                         "components/jquery/dist/jquery.js"
-                         "components/bootstrap/dist/js/bootstrap.js"
-                         "components/typeahead.js/dist/typeahead.jquery.js"
-                         "components/react/react.js"
-                         "cljs-target/goog/base.js"
-                         "main.js"))
+            (apply as-static include-css (if is-debug
+                                           debug-css
+                                           production-css))
+            (apply as-static include-js (if is-debug
+                                          debug-js
+                                          production-js))
             [:script "goog.require('subman.core');"]]
            [:body [:script "subman.core.run();"]])))
