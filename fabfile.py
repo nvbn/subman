9fd3cc260d6d2a4b2be75256782cf43db9138407
nvbn <nvbn.rm@gmail.com>
2014-09-23 20:10:36 +0400

    Update project dependencies

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ from fabric.api import cd, sudo, shell_env, hide
 def update():
     with cd('/var/www/subman/'), shell_env(HOME='/var/www/'), hide('stdout', 'stderr'):
         sudo('git pull', user='www-data')
+        sudo('lein bower install', user='www-data')
         sudo('rm -rf resources/public/cljs-target')
         sudo('rm -rf target/cljs*')
         sudo('lein with-profile production ring uberjar', user='www-data')
--- project.clj
+++ project.clj
@@ -4,36 +4,36 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [org.clojure/clojurescript "0.0-2322"]
-                           [compojure "1.1.8"]
+                           [org.clojure/clojurescript "0.0-2342"]
+                           [compojure "1.1.9"]
                            [hiccup "1.0.5"]
                            [enlive "1.1.5"]
                            [clojurewerkz/elastisch "2.0.0"]
                            [cljs-http "0.1.16"]
                            [clj-http "1.0.0"]
-                           [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                           [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                            [overtone/at-at "1.2.0"]
                            [garden "1.2.1"]
                            [ring "1.3.1"]
                            [swiss-arrows "1.0.0"]
                            [jayq "2.5.2"]
-                           [om "0.7.1"]
+                           [om "0.7.3"]
                            [environ "1.0.0"]
                            [test-sugar "2.1"]
                            [alandipert/storage-atom "1.2.3"]
-                           [secretary "1.2.0"]
+                           [secretary "1.2.1"]
                            [sablono "0.2.22"]
                            [prismatic/om-tools "0.3.3"]
-                           [com.cognitect/transit-clj "0.8.247"]
-                           [com.cognitect/transit-cljs "0.8.184"]
+                           [com.cognitect/transit-clj "0.8.259"]
+                           [com.cognitect/transit-cljs "0.8.188"]
                            [ring-transit "0.1.2"]
-                           [org.clojure/tools.logging "0.3.0"]
+                           [org.clojure/tools.logging "0.3.1"]
                            [com.cemerick/url "0.1.1"]
                            [itsy "0.1.1"]]
             :plugins [[lein-cljsbuild "1.0.3"]
-                      [com.keminglabs/cljx "0.3.2"]
-                      [lein-garden "0.1.8"]
-                      [lein-environ "0.5.0"]
+                      [com.keminglabs/cljx "0.4.0"]
+                      [lein-garden "0.2.1"]
+                      [lein-environ "1.0.0"]
                       [lein-ring "0.8.11"]
                       [lein-ancient "0.5.5"]
                       [com.cemerick/clojurescript.test "0.3.1"]
@@ -88,15 +88,16 @@
                             {:source-paths ["src/cljx"]
                              :output-path "target/generated-cljs"
                              :rules :cljs}]}
-            :garden {:builds [{:stylesheet subman.web.style/main
+            :garden {:builds [{:source-paths ["src/clj"]
+                               :stylesheet subman.web.style/main
                                :compiler {:output-to "resources/public/main.css"}}]}
             :ring {:handler subman.handlers/app
                    :init subman.handlers/init}
             :bower {:directory "resources/public/components"}
             :bower-dependencies [["bootstrap" "3.2.0"]
-                                 ["font-awesome" "4.1.0"]
+                                 ["font-awesome" "4.2.0"]
                                  ["jquery" "2.1.1"]
                                  ["typeahead.js" "0.10.5"]
                                  ["typeahead.js-bootstrap3.less" "develop"]
-                                 ["react" "0.11.1"]
+                                 ["react" "0.11.2"]
                                  ["es5-shim" "4.0.3"]])
