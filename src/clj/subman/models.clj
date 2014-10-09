d242e04ceb0ffa509e3a1871a965fc79511d4ccb
nvbn <nvbn.rm@gmail.com>
2014-10-09 18:49:33 +0400

    Use clj-di for db connection

--- src/clj/subman/models.clj
+++ src/clj/subman/models.clj
@@ -5,28 +5,28 @@
             [clojurewerkz.elastisch.rest.document :as esd]
             [clojurewerkz.elastisch.query :as q]
             [environ.core :refer [env]]
+            [clj-di.core :refer [register! get-dep]]
             [subman.helpers :as helpers :refer [defsafe]]
             [subman.const :as const]))
-
-(def connection (atom nil))
 
 (defn connect!
   "Connect to elastic"
   []
-  (reset! connection
-          (esr/connect (env :db-host))))
+  (register! :db-connection (esr/connect (env :db-host))))
 
 (defn get-total-count
   "Update total count of subtitles"
   []
-  (-> (esd/search @connection (env :index-name) "subtitle" :filter {})
+  (-> (esd/search (get-dep :db-connection)
+                  (env :index-name) "subtitle" :filter {})
       :hits
       :total))
 
 (defsafe create-index!
   "Create database index for subtitles"
   []
-  (esi/create @connection (env :index-name)
+  (esi/create (get-dep :db-connection)
+              (env :index-name)
               :mappings {"subtitle"
                          {:properties {:show {:type "string"}
                                        :season {:type "string"
@@ -43,12 +43,14 @@
 (defsafe create-document!
   "Put document into elastic"
   [doc]
-  (esd/create @connection (env :index-name) "subtitle" doc))
+  (esd/create (get-dep :db-connection)
+              (env :index-name) "subtitle" doc))
 
 (defn delete-all!
   "Delete all documents"
   []
-  (esd/delete-by-query-across-all-types @connection (env :index-name) (q/match-all)))
+  (esd/delete-by-query-across-all-types (get-dep :db-connection)
+                                        (env :index-name) (q/match-all)))
 
 (defn- get-season-episode-parts
   "Get season episode parts"
@@ -93,7 +95,7 @@
 (defn search
   "Search for documents"
   [& {:keys [query offset lang source]}]
-  (->> (apply esd/search @connection
+  (->> (apply esd/search (get-dep :db-connection)
               (env :index-name)
               "subtitle"
               :from offset
@@ -105,11 +107,10 @@
 (defn in-db
   "Check subtitle already in db"
   [subtitle]
-  (-> (let [url (:url subtitle)]
-        (esd/search @connection
-                    (env :index-name)
-                    "subtitle"
-                    :filter (q/term :url url)))
+  (-> (esd/search (get-dep :db-connection)
+                  (env :index-name)
+                  "subtitle"
+                  :filter (q/term :url (:url subtitle)))
       :hits
       :total
       (> 0)))
@@ -117,7 +118,7 @@
 (defn list-languages
   "List languages with count"
   []
-  (-> (esd/search @connection
+  (-> (esd/search (get-dep :db-connection)
                   (env :index-name)
                   "subtitle"
                   :query (q/match-all)
@@ -129,7 +130,7 @@
 
 (defn get-show-season-episode-set
   [from]
-  (->> (esd/search @connection
+  (->> (esd/search (get-dep :db-connection)
                    (env :index-name)
                    "subtitle"
                    :fields [:show :season :episode]
